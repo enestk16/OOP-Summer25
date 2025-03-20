d67f37f1d2d2328ff1b9a6da95558fd9f6f3243f
enestk16 <enestalhakanar@icloud.com>
2025-03-20 11:20:12 +0000

    Add new file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-# change it to print your name
-
-print("Hello, I am Piotr Brudny")
+animals = [{ 'name' : 'lion', 'group': 'mammal','number_of_legs': 4, 'skills': ['hunting','running'] ,
+            
+            'name' : 'dog', 'group': 'mammal','number_of_legs': 4, 'skills': ['hunting','running'],
+            'name' : 'Eagle', 'group': 'bird','number_of_legs': 2, 'skills': ['hunting','flying'],
+            'name' : 'shark', 'group': 'fish','number_of_legs': 0, 'skills': ['hunting','swimming']}]
