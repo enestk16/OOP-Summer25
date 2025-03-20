7df619c7b7671893839e723aec3295265dea55f3
enestk16 <enestalhakanar@icloud.com>
2025-03-20 11:26:46 +0000

    Add new file

---
+++ animal.py
@@ -0,0 +1,5 @@
+animals = [{ 'name' : 'lion', 'group': 'mammal','number_of_legs': 4, 'skills': ['hunting','running'] ,
+            
+            'name' : 'dog', 'group': 'mammal','number_of_legs': 4, 'skills': ['hunting','running'],
+            'name' : 'Eagle', 'group': 'bird','number_of_legs': 2, 'skills': ['hunting','flying'],
+            'name' : 'shark', 'group': 'fish','number_of_legs': 0, 'skills': ['hunting','swimming']}]
