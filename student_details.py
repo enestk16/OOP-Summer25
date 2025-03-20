9e0cf4e1020ff2f8bdccf85a08b93b9d7f0d0eea
enestk16 <enestalhakanar@icloud.com>
2025-03-20 12:09:03 +0000

    new

---
+++ student_details.py
@@ -0,0 +1,8 @@
+name:studen_data
+set-student-data:
+ first_name :nusura
+ second_name :niyomukiza 
+ index_number :35435
+ nationality : rwandan
+ starting_date : 01/10/2024
+ courses : computer programing
