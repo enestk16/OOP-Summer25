18b5dfcb0f95c13ae51211de1fc6971e0dc6ec3d
enestk16 <enestalhakanar@icloud.com>
2025-03-26 19:59:03 +0000

    new

---
+++ animal1.py
@@ -0,0 +1,36 @@
+class Animals:
+    def __init__(self,name,group,legs,skills):
+        self.name=name
+        self.group=group
+        self.legs=legs
+        self.skills=skills
+
+a1=Animals("cat","mammals",4,"4 leg stand when fell")
+print(a1.name)
+print(a1.group)
+print(a1.legs)
+print(a1.skills)
+
+a2=Animals("dog","mammals",4,"hypersensitivity")
+print(a2.name)
+print(a2.group)
+print(a2.legs)
+print(a2.skills)
+
+a3=Animals("monkey","mammals",2,"ability to walk on 2 feet")
+print(a3.name)
+print(a3.group)
+print(a3.legs)
+print(a3.skills)
+
+a4=Animals("cow","mammals",4,"producing milk")
+print(a4.name)
+print(a4.group)
+print(a4.legs)
+print(a4.skills)
+
+a5=Animals("mosquito","insect",4,"nothing")
+print(a5.name)
+print(a5.group)
+print(a5.legs)
+print(a5.skills)
