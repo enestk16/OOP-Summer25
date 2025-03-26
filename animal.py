9421930f30ecdc321812e2db4647163a87912d9c
enestk16 <enestalhakanar@icloud.com>
2025-03-26 19:35:28 +0000

    add animal

--- animal.py
+++ animal.py
@@ -1,2 +1,12 @@
+animal1={"name":"Cat","group":"Mammals","legs":4,"skills":"4 leg stand when fell"}
+animal2={"name":"dog","group":"Mammals","legs":4,"skills":"hypersensitivity"}
+animal3={"name":"monkey","group":"mammals","legs":2,"skills":"ability to walk on 2 feet"}
+animal4={"name":"cow","group":"mammals","legs":4,"skills":"producing milk"}
+animal5={"name":"mosquito","group":"insect","legs":4,"skills":"nothing"}
 
+print(animal1["name"],animal1["group"],animal1["legs"],animal1["skills"])
+print(animal2["name"],animal2["group"],animal2["legs"],animal2["skills"])
+print(animal3["name"],animal3["group"],animal3["legs"],animal3["skills"])
+print(animal4["name"],animal4["group"],animal4["legs"],animal4["skills"])
+print(animal5["name"],animal5["group"],animal5["legs"],animal5["skills"])
 
