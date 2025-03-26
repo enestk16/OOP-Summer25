373ef09c29e92d1a244bc85ee2f34f75270947b9
enestk16 <enestalhakanar@icloud.com>
2025-03-26 19:09:37 +0000

    new

--- animal.py
+++ animal.py
@@ -1,30 +1,5 @@
-<<<<<<< HEAD
-animals = [{ 'name' : 'lion', 'group': 'mammal','number_of_legs': 4, 'skills': ['hunting','running'] ,
-            
-            'name' : 'dog', 'group': 'mammal','number_of_legs': 4, 'skills': ['hunting','running'],
-            'name' : 'Eagle', 'group': 'bird','number_of_legs': 2, 'skills': ['hunting','flying'],
-            'name' : 'shark', 'group': 'fish','number_of_legs': 0, 'skills': ['hunting','swimming']}]
-=======
-animal1 = [
-    {"name": "Cat",
-        "group": "Mammals",
-        "number_of_legs": 4,
-        "skills": ["jumping", "climbing"] },
-    { "name": "Eagle",
-        "group": "Birds",
-        "number_of_legs": 2,
-        "skills": ["flying", "hunting"]},
-    {"name": "Frog",
-        "group": "Amphibians",
-        "number_of_legs": 4,
-        "skills": ["jumping", "swimming"] },
-    {"name": "Spider",
-        "group": "Arachnids",
-        "number_of_legs": 8,
-        "skills": ["web-spinning", "climbing"]},
-    {"name": "Snake",
-        "group": "Reptiles",
-        "number_of_legs": 0,
-        "skills": ["slithering", "camouflage"]}]
-print(animals)
->>>>>>> 56f7433 (new changes)
+class Person:
+    def __init__(self,name,age,adress):
+        self.name=name
+        self.age=age
+        self.adress=adress
