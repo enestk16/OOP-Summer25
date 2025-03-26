016d092a2ab017bf50fbada2d453237698458d24
enestk16 <enestalhakanar@icloud.com>
2025-03-26 19:51:37 +0000

    nn

---
+++ student_details_structure.py
@@ -0,0 +1,14 @@
+student1={
+"first_name": "enes talha",
+"last_name" :"kanar",
+"index_number" : "35375",
+"nationality" : "turkish",
+"starting_date" : "13/09/2024",
+"courses" : ["econ","math"]
+}
+print(student1["first_name"])
+print(student1["last_name"])
+print(student1["index_number"])
+print(student1["nationality"])
+print(student1["starting_date"])
+print(student1["courses"])
