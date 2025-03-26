65fda8198111aae5150d5552a494f6c2db123e38
enestk16 <enestalhakanar@icloud.com>
2025-03-26 19:17:23 +0000

    finished

--- animal.py
+++ animal.py
@@ -22,3 +22,15 @@ print(s1.last_name)
 print(s1.index_number)
 print(s1.nationality)
 
+s2=Student("Can","Kitapçı",34598,"Turkish")
+print(s2.name)
+print(s2.last_name)
+print(s2.index_number)
+print(s2.nationality)
+
+s3=Student("Beyza","Koç",45673,"Turkish")
+print(s3.name)
+print(s3.last_name)
+print(s3.index_number)
+print(s3.nationality)
+
